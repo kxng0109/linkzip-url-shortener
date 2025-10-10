7eb1205426482a7ca0abd673a413c2558b5103b1
Joshua Ike <79171733+kxng0109@users.noreply.github.com>
2025-10-11 00:02:54 +0100

    Initial commit

---
+++ src/main/java/io/github/kxng0109/linkzip/LinkzipApplication.java
@@ -0,0 +1,13 @@
+package io.github.kxng0109.linkzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkzipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkzipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kxng0109/linkzip/LinkzipApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kxng0109.linkzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
